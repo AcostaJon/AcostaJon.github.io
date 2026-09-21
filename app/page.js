ac80b2e081ba198eb934f91877acd9c3c1906db4
Jonathan Acosta <jonacosta86@gmail.com>
2026-09-21 12:02:58 -0400

    project update

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
         key: `a${aKeyValue++}`,
         id: `reelBuzzProject`,
         title: "ReelBuzz | Media Streaming",
-        description: "ReelBuzz brings the freshest movie and TV game-changer trailers right to your fingertips—whether you're tracking the latest blockbuster, binge-worthy series, or indie gem",
+        description: "ReelBuzz is a sleek, responsive web application built to serve as a central hub for official movie and TV show trailers.",
         languages: ["Reactjs", "Nextjs", "Css", "ReactPlayer", "MongoDB"],
         githubLink: "https://github.com/AcostaJon/ReelBuzz",
         projectLink: "https://reel-buzz.vercel.app/",
@@ -71,116 +71,7 @@ export default function Home() {
     ]
   );
   
-  // static projects
-  const [staticProjects, setStaticProjects] = useState(
-    [
-      {
-        key: `s${sKeyValue++}`,
-        id: `magentoProject`,
-        title: "Magento",
-        description: "Business landing page for a leader in digital commerce",
-        languages: ['Html', 'Css'],
-        githubLink: "https://github.com/AcostaJon/Magento",
-        projectLink: "/Magento"
-      },
-      {
-        key: `s${sKeyValue++}`,
-        id: `blossomKitchenProject`,
-        title: "Blossom Kitchen",
-        description: "'Eat green, stay healthy is the slogan' check out their landing page",
-        languages: ['Html', 'Css'],
-        githubLink: "https://github.com/AcostaJon/Blossom-Kitchen",
-        projectLink: "/Blossom-Kitchen"
-      },
-      {
-        key: `s${sKeyValue++}`,
-        id: `webStyleGuideProject`,
-        title: "WebStyle Guide",
-        description: "Guidelines that display the conversion of css to sass through partials",
-        languages: ['Html', 'Css', 'Sass'],
-        githubLink: "https://github.com/AcostaJon/techdegree-project-4",
-        projectLink: "/techdegree-project-4"
-      },
-      {
-        key: `s${sKeyValue++}`,
-        id: `registrationFormProject`,
-        title: "Registration Form",
-        description: "Commonly used registration form for acquiring new users",
-        languages: ['Html', 'Css'],
-        githubLink: "https://github.com/AcostaJon/My-techdegree-project-3",
-        projectLink: "/My-techdegree-project-3/"
-      },
-      {
-        key: `s${sKeyValue++}`,
-        id: `studentPortfolioProject`,
-        title: "Student Portfolio",
-        description: "A single page user portfolio",
-        languages: ['Html', 'Css'],
-        githubLink: "https://github.com/AcostaJon/techdegree-project-2",
-        projectLink: "/techdegree-project-2/"
-      },
-      {
-        key: `s${sKeyValue++}`,
-        id: `clientResumeProject`,
-        title: "Client Resume",
-        description: "A single page resume for a client",
-        languages: ['Html', 'Css'],
-        githubLink: "https://github.com/AcostaJon/techdegree-project-1",
-        projectLink: "/techdegree-project-1/"
-      }
-    ]
-  );
 
-  // dynamic projects
-  const [dynamicProjects, setDynamicProjects] = useState(
-    [
-      {
-        key: `d${dKeyValue++}`,
-        id: `businessConsultProject`,
-        title: "James Consulting",
-        description: "Heres a business web site for a start-up consulting company ",
-        languages: ['Nextjs', 'Reactjs', "Bootstrap", "Css"],
-        githubLink: "https://github.com/AcostaJon/Consulting-Firm",
-        projectLink: "/Consulting-Firm"
-      },
-      {
-        key: `d${dKeyValue++}`,
-        id: `skyLineProject`,
-        title: "Skyline Travels",
-        description: "Landing page for a travel agency",
-        languages: ['Html', 'Css', 'Javascript'],
-        githubLink: "https://github.com/AcostaJon/Skyline",
-        projectLink: "/Skyline"
-      },
-      {
-        key: `d${dKeyValue++}`,
-        id: `userDashboardProject`,
-        title: "User Dashboard",
-        description: "A back-end user dashboard, includes scalable vector graphics and js graph plugins to display data",
-        languages: ['Javascript', 'Html', "Css"],
-        githubLink: "https://github.com/AcostaJon/techdegree-project-7",
-        projectLink: "/techdegree-project-7"
-      },
-      {
-        key: `d${dKeyValue++}`,
-        id: `gameShowProject`,
-        title: "Game Show",
-        description: "A word guessing game where players will click letters from an onscreen keyboard to try to guess a random phrase",
-        languages: ['Javascript', 'Html', "Css"],
-        githubLink: "https://github.com/AcostaJon/techdegree-project-6",
-        projectLink: "/techdegree-project-6"
-      },
-      {
-        key: `d${dKeyValue++}`,
-        id: `photoGalleryProject`,
-        title: "Photo Gallery",
-        description: "A showcase of photos. Search for whats written in the photos captions and out comes that photo",
-        languages: ['Javascript', 'Html', "Css"],
-        githubLink: "https://github.com/AcostaJon/techdegree-project-5",
-        projectLink: "/techdegree-project-5"
-      }
-    ]
-  );
 
   return (
     <div className={styles.home}>
@@ -191,7 +82,7 @@ export default function Home() {
       </div>
       <Header />
       <LeftAside />
-      <Main apiAccordion={apiProjects} staticAccordion={staticProjects} dynamicAccordion={dynamicProjects} />
+      <Main apiAccordion={apiProjects} />
       <RightAside />
     </div>
   )
